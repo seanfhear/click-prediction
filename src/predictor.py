2e1b02a418d249dea596b946478e9e8d7ba28a60
Sean-Hassett <hassetse@tcd.ie>
2019-11-19 15:40:24 +0000

    trying random forest

--- src/predictor.py
+++ src/predictor.py
@@ -1,10 +1,8 @@
-import numpy as np
 import pandas as pd
 from sklearn import svm
 from sklearn import metrics
 from sklearn.model_selection import train_test_split
-import optunity
-import optunity.metrics
+from sklearn.ensemble import RandomForestClassifier
 from configparser import ConfigParser
 from datetime import datetime
 from src import preprocess
@@ -19,14 +17,17 @@ def train(jabref_train, myvolts_train, homepage_train):
     myvolts_y = myvolts_train[DEFAULTS['Target']]
     myvolts_X = myvolts_train.drop(DEFAULTS['TrainingTargets'].split(','), axis=1)
     myvolts_X_train, myvolts_X_test, myvolts_y_train, myvolts_y_test = \
-        train_test_split(myvolts_X, myvolts_y, test_size=0.3, random_state=109)
+        train_test_split(myvolts_X, myvolts_y, test_size=0.2, random_state=109)
 
-    clf = svm.SVC(
-        kernel=DEFAULTS['Kernel'],
-        C=10 ** float(DEFAULTS['C']),
-        gamma=10 ** float(DEFAULTS['Gamma']),
-        degree=int(DEFAULTS['Degree'])
-    )
+    # clf = svm.SVC(
+    #     kernel=DEFAULTS['Kernel'],
+    #     C=float(DEFAULTS['C']),
+    #     gamma=float(DEFAULTS['Gamma']),
+    #     degree=int(DEFAULTS['Degree'])
+    # )
+
+    clf = RandomForestClassifier(random_state=1, n_estimators=400, class_weight='balanced')
+
     clf.fit(myvolts_X_train, myvolts_y_train)
     y_pred = clf.predict(myvolts_X_test)
 
@@ -37,27 +38,6 @@ def train(jabref_train, myvolts_train, homepage_train):
     print('Precision: ', metrics.precision_score(myvolts_y_test, y_pred))
     print('Recall: ', metrics.recall_score(myvolts_y_test, y_pred))
     print('Time Taken: ', datetime.now() - start_time)
-
-
-def tune_hyperparams(jabref_train, myvolts_train, homepage_train):
-    print('starting...')
-    start_time = datetime.now()
-    myvolts_y = myvolts_train[DEFAULTS['Target']].to_numpy()
-    myvolts_X = myvolts_train.drop(DEFAULTS['TrainingTargets'].split(','), axis=1).to_numpy()
-
-    @optunity.cross_validated(x=myvolts_X, y=myvolts_y, num_folds=10, num_iter=1)
-    def svm_auc(x_train, y_train, x_test, y_test, logC, logGamma):
-        print('iteration...')
-        model = svm.SVC(C=10 ** logC, gamma=10 ** logGamma).fit(x_train, y_train)
-        decision_values = model.decision_function(x_test)
-        return optunity.metrics.roc_auc(y_test, decision_values)
-
-    print('tuning...')
-    hps, _, _ = optunity.maximize(svm_auc, num_evals=10, logC=[-5, 2], logGamma=[-5, 1])
-
-    print("Optimal C: ", hps['logC'])
-    print('Optimal Gamma: ', hps['logGamma'])
-    print("Time Taken: ", datetime.now() - start_time)
 
 
 def predict(train, test):
